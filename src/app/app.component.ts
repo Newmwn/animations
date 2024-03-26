d22e211002b12efc703493155496838f272b29e3
Newmwn <96746475+Newmwn@users.noreply.github.com>
2024-03-26 00:07:25 +0000

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'animations';
+  entrie:string='';
+  class:string='';
+  class2:string='';
+  class3:string='';
+  timeDelay:number=0
+  duration:number=1
+
+  clickE(entrie:string){
+   this.class = '';
+   setTimeout(() => {
+     this.class = entrie
+   });
+  }
+
+  clickEph(entrie:string){
+   this.class2 = '';
+   setTimeout(() => {
+     this.class2 = entrie
+   });
+  }
+
+  clickL(entrie:string){
+   this.class3 = '';
+   setTimeout(() => {
+     this.class3 = entrie
+   });
+  }
 }
